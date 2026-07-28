976276eb0315a07b8ab1ed492978ef1f8651fb59
mouhamednd-crypto <mouhamednd775346130@gmail.com>
2026-07-28 16:01:45 +0000

    Test variables

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,5 +1,13 @@
 import nodemailer from "nodemailer";
 
+if (!process.env.EMAIL_USER) {
+  throw new Error("EMAIL_USER est manquant");
+}
+
+if (!process.env.EMAIL_PASS) {
+  throw new Error("EMAIL_PASS est manquant");
+}
+
 export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
